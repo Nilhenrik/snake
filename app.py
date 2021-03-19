d35060b690884f2aecebba2848e10cef34d08205
Jonas Emanuel Gilje <jonas@jonasgilje.no>
2021-03-19 11:12:27 +0100

    a basic ai is now working

--- app.py
+++ app.py
@@ -20,16 +20,29 @@ def next_state():
         game.restart()
         p1 = "-1"
         p2 = "-1"
-    if p1 != "-1":
-        game.snakes[0].dir = int(p1)
+    else:
+        
+    #if p1 != "-1":
+        #game.snakes[0].dir = #int(p1)
+        #game.snakes[1].dir = #(2 + int(p1)) % 4
+        for snake in game.snakes:
+            n_dir = next(snake.ai.gen)
+            if n_dir != -1:
+                snake.dir = n_dir#next(snake.ai.gen)
         p1 = "-1"
-    if p2 != "-1":
-        game.snakes[1].dir = int(p2)
-        p1 = "-1"
+    #if p2 != "-1":
+    #    game.snakes[1].dir = int(p2)
+    #    p1 = "-1"
     while True:
         try:
-            state_obj = {"board": next(game.gen).tolist()}
+            state_obj = {
+                "board": next(game.gen).tolist(),
+                "player1": [game.snakes[0].x, game.snakes[0].y],
+                "player2": [game.snakes[1].x, game.snakes[1].y],
+                "message": game.message,
+                }
+            game.message = ""
             break
-        except (StopIteration, RuntimeError):
+        except StopIteration:
             game.restart()
     return json.dumps(state_obj)
--- board.py
+++ board.py
@@ -17,14 +17,17 @@ class Board:
         start_dir = random.randrange(4) # N = 0, E = 1, S = 2, W  = 3
         start_dir2 = (start_dir + 2) % 4
 
-        self.snakes.append(Snake(start, len(self.snakes)+1, start_dir))
-        self.snakes.append(Snake(start2, len(self.snakes)+1, start_dir2))
+        self.snakes.append(Snake(start, len(self.snakes)+1, start_dir, self.matrix, self))
+        self.snakes.append(Snake(start2, len(self.snakes)+1, start_dir2, self.matrix, self))
         self.vinner = 0
 
         self.gen = self.game()
+        self.message = ""
 
     def restart(self):
+        message = self.message
         self.__init__()
+        self.message = message
 
 
 
@@ -54,36 +57,85 @@ class Board:
             player = 0
             for snake in self.snakes:
                 player += 1
-                if snake.dir == 0:
-                    snake.y -= 1
-                if snake.dir == 1:
-                    snake.x += 1
-                if snake.dir == 2:
-                    snake.y += 1
-                if snake.dir == 3:
-                    snake.x -= 1
+                if   snake.dir == 0: snake.y -= 1
+                elif snake.dir == 1: snake.x += 1
+                elif snake.dir == 2: snake.y += 1
+                elif snake.dir == 3: snake.x -= 1
                 
                 if self.checkCrash(snake):
-                    print("ferdig")
+                    self.message += "ferdig, "
                     if self.vinner == 0:
                         self.vinner = 3-player
                     else:
                         self.vinner = 3     
-                    print(f"{self.vinner} har vunnet")
-                    raise StopIteration()
+                    self.message += f"{self.vinner} har vunnet\n"
+                    return
                 else:
                     self.matrix[snake.y][snake.x] = True   
             yield self.matrix                 
 
 
 class Snake:
-    def __init__(self, start, player, dir):
+    def __init__(self, start, player, dir, matrix, game):
         self.y = start[0]
         self.x = start[1]
         #if player == 2:
             #self.x += 10
-        self.dir = dir
+        self.__dir = dir
+        self.ai = RandomAI(self, matrix)
+        self.game = game
+        self.player = player
+    
+    @property
+    def dir(self):
+        return self.__dir
+    
+    @dir.setter
+    def dir(self, new_dir):
+        if not new_dir in range(4):
+            raise ValueError("illegal direction")
+        if (2 + new_dir) % 4 == self.dir:
+            return
+        self.__dir = new_dir 
+
+
+class RandomAI:
+    def __init__(self, snake, matrix):
+        self.snake = snake
+        self.matrix = matrix
+        self.gen = self.generator()
+
+    def generator(self):
+        while True:
+            choices = [-1, -1,  (self.snake.dir + 1) % 4, (self.snake.dir + 3) % 4]
+            while choices:
+                choice_index = random.randrange(len(choices))
+                choice = choices[choice_index]
+                bad_choice = False
+                x, y = self.snake.x, self.snake.y
+                dir = self.snake.dir if choice == -1 else choice
+                if   dir == 0: y -= 1
+                elif dir == 1: x += 1
+                elif dir == 2: y += 1
+                elif dir == 3: x -= 1
+                try:
+                    if self.matrix[y, x]:
+                        bad_choice = True
+                except IndexError:
+                    bad_choice = True
+                if bad_choice:
+                    del choices[choice_index]
+                    self.snake.game.message += f"player {self.snake.player}: "
+                    self.snake.game.message += f"sletta {choice_index}, "
+                    self.snake.game.message += f"gjenværende {choices}\n"
+                    continue
+                yield choice
+                break
+            if not choices:
+                self.snake.game.message += f"player {self.snake.player} will lose\n"
+                yield -1
 
 if __name__ == "__main__":
     game = Board()
     game.game()
+
